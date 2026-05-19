f527fa346f1445bdf4a55a1c285adebdd0484807
Nithish-107 <nithishh486@gmail.com>
2026-05-19 19:43:19 +0530

    Fixed register CSS import path

--- crm-frontend/src/pages/Register.jsx
+++ crm-frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Register.css";
+import "../css/Register.css";
 
 function Register() {
   const navigate = useNavigate();
